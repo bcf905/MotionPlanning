69ffc094ebbf27d6e22ac16f0a0fd29c13d3fae1
bcf905 <bcf905@alumni.ku.dk>
2024-07-29 22:37:23 +0200

    Read from file

---
+++ Auxiliary/GCode.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Shapes;
+using MotionPlanning.Job;
+using MotionPlanning.Statements;
+
+namespace MotionPlanning.Auxiliary
+{
+    public class GCode
+    {
+        public static Job.Job Read(StreamReader reader)
+        {
+            Job.Job job = new();
+            string? line;
+
+            while ((line = reader.ReadLine()) != null )
+            {
+                IURScript statement = Identifier.Identify(line);
+                job.AddStatement(statement);
+            }
+
+            return job;
+        }
+    }
+}
