59ba98154355933f444fbf47cf91e146d08a9e42
bcf905 <bcf905@alumni.ku.dk>
2024-07-31 13:24:01 +0200

    Coordinate added

---
+++ Coordinates/Coordinate2D.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotionPlanning.Coordinates
+{
+    public class Coordinate2D
+    {
+        public Coordinate2D(float x, float y) 
+        {
+            this.X = x; 
+            this.Y = y;
+        }
+
+        public (float, float) GetCoordinate()
+        {
+            return (this.X, this.Y);
+        }
+        float X { get; set; }
+        float Y { get; set; }
+
+    }
+}
